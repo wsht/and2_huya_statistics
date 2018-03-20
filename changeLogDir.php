2fd09ef4ba71f9905af6bd74452abb8605d698c9
wsht <952587282@qq.com>
2018-03-20 15:10:27 +0800

    chagne log dir

--- changeLogDir.php
+++ changeLogDir.php
@@ -10,9 +10,46 @@
 $targetDir = "/root/wsht/and2_huya_statistics/";
 
 $fileListCmd = "ls $targetDir | egrep 'message.*.log'";
-
 $fileList = `$fileListCmd`;
-
 $fileList = explode("\n", $fileList);
 
-var_dump($fileList);
+class TargetDirDefine
+{
+	const ROOT_DIR    = '/data/huya_log/v1';
+	const MESSAGE_DIR = self::ROOT_DIR . "/message/";
+	const ONLINE_DIR  = self::ROOT_DIR . "/online/";
+	const GIFT_DIR    = self::ROOT_DIR . "/gift/";
+
+	static function writeLog($dir, $message, $timestamp)
+	{
+		$timestamp = intval($timestamp / 1000);
+		$date = date("Y-m-d", $timestamp);
+		$logName = $date . ".log";
+		$logDir = $dir . $logName;
+
+		file_put_contents($logDir, $message, FILE_APPEND);
+	}
+}
+
+
+foreach ($fileList as $file) {
+	$filePath = $targetDir . $file;
+	echo "copy $filePath start\n";
+	$handle = fopen($filePath, "r");
+	while ($buffer = fgets($handle, 4096)) {
+		$message = json_decode($buffer);
+		switch ($message->type) {
+			case "chat":
+				TargetDirDefine::writeLog(TargetDirDefine::MESSAGE_DIR, $buffer, $message->time);
+				break;
+			case "gift":
+				TargetDirDefine::writeLog(TargetDirDefine::GIFT_DIR, $buffer, $message->time);
+				break;
+			case "online":
+				TargetDirDefine::writeLog(TargetDirDefine::ONLINE_DIR, $buffer, $message->time);
+				break;
+		}
+	}
+	echo "copy $filePath finish \n\n";
+	fclose($handle);
+}
