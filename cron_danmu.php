19d35965d48f575b814bb295f55d8b886f11fd09
wsht <952587282@qq.com>
2018-04-08 16:03:31 +0800

    test

--- cron_danmu.php
+++ cron_danmu.php
@@ -9,8 +9,9 @@ echo "current run num is $runNum\n";
 if($runNum >= 1){
     echo "runnum is:".$runNum."\n";
     exit(0);
+}else
+{
+    $cmd = '(node /root/wsht/and2_huya_statistics/danmu.js xxm 2058731947 >> /data/huya_log/v1/error.log &)';
+
+    echo `$cmd`;
 }
-
-$cmd = '(node /root/wsht/and2_huya_statistics/danmu.js xxm 2058731947 >> /data/huya_log/v1/error.log &)';
-
-echo `$cmd`;
