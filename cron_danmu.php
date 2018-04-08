324ac6a671fb008a4bd5b71ee2dbf3551220e428
wsht <952587282@qq.com>
2018-04-08 15:20:07 +0800

    cron monitor node danmujs

--- cron_danmu.php
+++ cron_danmu.php
@@ -7,9 +7,10 @@ $runNum = intval(`$cmd`);
 
 
 if($runNum >= 1){
+    echo "runnum is:".$runNum."\n";
     exit(0);
 }
 
 $cmd = '(node danmu.js xxm 2058731947 >> /data/huya_log/v1/error.log &)';
 
-`$cmd`;
+echo `$cmd`;
